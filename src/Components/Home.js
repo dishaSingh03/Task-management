3e0e30adeb4208ad8634ea13cd63054beba361b7
disha@03 <disha.singhh@gmail.com>
2023-07-04 13:17:28 +0530

    initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "../CSS/Home.css";
+
+const Home = () => {
+  const [isMenuOpen, setMenuOpen] = useState(true);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const logout = () => {
+    window.location.href = "/";
+  };
+
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            TASK MANAGEMENT
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link disabled"
+                  href="#"
+                  tabindex="-1"
+                  aria-disabled="true"
+                >
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input
+                id="home-search"
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+              &nbsp; &nbsp;
+              <button class="btn btn-success" type="submit" onClick={logout}>
+                LOGOUT
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <div className="Home-table">
+        <table class="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/Components/Login.js
@@ -0,0 +1,124 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import "../CSS/Login.css";
+
+const Login = () => {
+  // Creating schema
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email is a required field")
+      .email("Invalid email format"),
+    password: Yup.string()
+      .required("Password is a required field")
+      .min(8, "Password must be at least 8 characters"),
+  });
+
+  const [loginValues, setLoginValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [showAlert, setShowAlert] = useState(false);
+
+  const validateLogin = (values) => {
+    const Email = "task@gmail.com";
+    const password = "admin123";
+
+    if (values.email === Email && values.password === password) {
+      window.location.href = "/home";
+    } else {
+      setShowAlert(true);
+    }
+  };
+
+  const handleAlertDismiss = () => {
+    setShowAlert(false);
+  };
+
+  return (
+    <>
+      <Formik
+        validationSchema={schema}
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => {
+          // Alert the input values of the form that we filled
+
+          validateLogin(values);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => (
+          <div class="login-container">
+            <div class="login-card">
+              <h2 className="head">Login Form</h2>
+              <form className="login-form" noValidate onSubmit={handleSubmit}>
+                <label className="login-label" for="username">
+                  Email
+                </label>
+                <input
+                  className="login-input"
+                  type="email"
+                  id="email"
+                  placeholder="Enter your username"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                {/* If validation is not passed show errors */}
+                <p className="error">
+                  {errors.email && touched.email && errors.email}
+                </p>
+
+                <label className="login-label" for="password">
+                  Password
+                </label>
+                <input
+                  className="login-input"
+                  type="password"
+                  id="password"
+                  placeholder="Enter your password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                <p className="error">
+                  {errors.password && touched.password && errors.password}
+                </p>
+                <br />
+
+                <button className="login-btn" type="submit">
+                  Login
+                </button>
+              </form>
+              <br />
+              {showAlert && (
+              <div className="alert alert-danger alert-dismissible" role="alert">
+                <strong>Danger!</strong> Wrong email or password!
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="alert"
+                  aria-label="Close"
+                  // onClick={handleAlertDismiss}
+                ></button>
+              </div>
+            )}
+            </div>
+          </div>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default Login;
